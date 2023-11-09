8ba07ecd49d813ec863ecfb3d35c2b9797df469e
Guoning-Chen <cgn1874@163.com>
2023-11-09 20:23:28 +0800

    添加opencv头文件

--- src/HW1/HW1.cpp
+++ src/HW1/HW1.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs/legacy/constants_c.h>
+#include <opencv2/imgproc/types_c.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include "utils.h"
